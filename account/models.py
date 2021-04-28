595bfef50c0fb3382b2d2caf88087f637eb98d79
Maisim Zaman <zmais@gmail.com>
2021-04-27 22:03:17 -0400

    new

--- account/models.py
+++ account/models.py
@@ -45,7 +45,7 @@ def get_background_image_filepath(self, filename):
 	return 'static/background_images/' + str(self.pk) + '/profile_image.png'
 
 def get_default_profile_image():
-	return "static/profile_images/7/profile_image.png"
+	return ""
 
 
 class Account(AbstractBaseUser):
